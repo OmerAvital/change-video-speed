3e13ff42a77a7c874ec4e3df0fcad3eda5408ee5
Omer Avital <omer.avital@icloud.com>
2021-08-30 19:28:37 -0700

    color changes

--- src/colorScheme.js
+++ src/colorScheme.js
@@ -8,21 +8,26 @@ let preferredColorScheme = window.matchMedia("(prefers-color-scheme: dark)")
 
 try {
   chrome.storage.sync.get(["colorScheme"], ({ colorScheme }) => {
+    if (colorScheme === undefined) {
+      throw new Error("Color scheme not saved.");
+    }
+
     currentColorScheme = colorScheme;
     setColorScheme(currentColorScheme);
   });
 } catch (e) {
-  console.error(e);
-  chrome.storage.sync.set({ colorScheme: "auto" }, () =>
-    setColorScheme("auto")
-  );
+  // setColorScheme("auto");
+  setColorScheme("dark");
+  chrome.storage.sync.set({ colorScheme: "auto" });
 }
 
 window
   .matchMedia("(prefers-color-scheme: dark)")
   .addEventListener("change", (e) => {
-    preferredColorScheme = e.matches ? "dark" : "light";
-    setColorScheme(currentColorScheme);
+    if (currentColorScheme === "auto") {
+      preferredColorScheme = e.matches ? "dark" : "light";
+      setColorScheme("auto");
+    }
   });
 
 function setColorScheme(scheme) {
