38c97d19e15f03348aebe7976c5b3898bb3e2d91
Omer Avital <76176305+OmerAvital@users.noreply.github.com>
2022-03-01 15:19:56 -0800

    Add tenser plugin (#4)

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,6 +1,7 @@
 import { Configuration } from 'webpack';
 import { merge } from 'webpack-merge';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import TenserPlugin from 'terser-webpack-plugin';
 import common from './webpack.common';
 
 export default merge<Configuration>(common, {
@@ -10,6 +11,7 @@ export default merge<Configuration>(common, {
     minimizer: [
       '...',
       new MiniCssExtractPlugin(),
+      new TenserPlugin(),
     ],
   },
 } as Configuration);
